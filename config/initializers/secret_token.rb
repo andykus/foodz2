48e4517d54bf474b4b6519e4089bce218f919716
Elin Nilsson <elinhejnilsson@gmail.com>
2013-05-30 18:34:58 +0200

    Testing testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodz2::Application.config.secret_token = 'ae88d93708aba258e9355a666b2b12149916ad4749c5cf9fbca3da18f391678c4223521c32e22795e77864f89ada0d79593cdb332ba75be26defdc0494b8dbca'
